74e5ef89b9da2175e5448d794ea07dbaeaa5fcde
Stevensurez <129130893+Stevensurez@users.noreply.github.com>
2023-09-24 21:14:07 -0500

    crup
    ajustes crup

--- server/controllers/albumController.js
+++ server/controllers/albumController.js
@@ -1,21 +1,75 @@
+const { json } = require('express');
 const album = require('../models/album');
 
-exports.getAlbumsByNombre = async(req, res) => {
+//Elimina album por Id
+exports.deleteAlbum = async(req, res) => {
+
+    try {
+        const albumOne = await album.findById(req.params.id) 
+        let _id = albumOne._id;
+        if(_id){
+            await album.findByIdAndDelete(_id)
+        }
+        res.status(200).json({message: 'Album eliminado exitosamente'})
+        
+    } catch (error) {
+        res.status(500).json({error: 'Album no encontrado'})
+    }
+}
+
+// Busca album por Id
+exports.getById = async(req, res) => {
+    try {
+        const albumOne = await album.findById(req.params.id)
+        res.json(albumOne)
+
+    } catch (err) {
+        res.status(404).json({err: 'Albun no encontrado'})
+    }
+}
+
+//Trae todos los albunes
+exports.getAlbums = async(req, res) => {
     try {
         const albums = await album.find()
-        const albumsByNombre = {}
-
-        albums.forEach((album) => {
-
-            if(!albumsByNombre[album.nombre]){
-                albumsByNombre[album.nombre] = []
-            }
-
-            albumsByNombre[album.nombre].push(album)
-        })
-
-        res.json(albumsByNombre)
+        res.json(albums)
     } catch (err) {
         res.status(500).json({err: 'Error al obtener los datos'})
+    }
+}
+
+//Crea un album
+exports.createAlbum = async(req, res) => {
+    try {
+        
+        const {nombre, artista, descripcion, imagen_url} = req.body;
+
+        const newAlbum = new album({
+            nombre,
+            artista,
+            descripcion,
+            imagen_url
+        })
+        const saveAlbum = await newAlbum.save()
+
+        res.status(201).json(saveAlbum)
+
+    } catch (err) {
+        res.status(500).json({err:'Error al crear los albunes'})
+        
+    }
+}
+
+exports.updateAlbum = async(req, res) => {
+    try {
+        
+        const {id} = req.params;
+        await album.updateOne({_id: id}, {...req.body})
+        const updateAlbum = await album.findById(id)
+        return res.status(200).json(updateAlbum)
+
+    } catch (err) {
+        res.status(500).json({err:'No se pudo modificar el album'})
+        
     }
 }
--- server/routers/routes.js
+++ server/routers/routes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const albumsController = require('../controllers/albumController');
 
 
-router.get('/albums', albumsController.getAlbumsByNombre)
+router.get('/', albumsController.getAlbums)
+router.get('/:id', albumsController.getById)
+router.post('/create', albumsController.createAlbum)
+router.patch('/:id', albumsController.updateAlbum)
+router.delete('/:id', albumsController.deleteAlbum)
+
 
 module.exports = router
